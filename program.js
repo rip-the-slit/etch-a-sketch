28be9a34c8e5ab67952f71c958fe9850a7642cef
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-06-29 15:32:50 -0430

    Set up createGrid function so that the box amount is modifiable

--- program.js
+++ program.js
@@ -1,11 +1,21 @@
 const container = document.querySelector(".container")
 
-for (let i = 0; i < 256; i++) {
+function createGrid (boxAmount = 16) {
+
+for (let i = 0; i < boxAmount*boxAmount; i++) {
     const box = document.createElement("div")
     box.setAttribute("class", "box")
+
+    let boxSize = (400 / boxAmount) - 2;
+    box.style.width = `${boxSize}px`
+    box.style.height = `${boxSize}px`
+
     box.addEventListener("mouseover", () => {
         box.classList.add("painted")
     })
     container.appendChild(box)
 }
 
+}
+
+createGrid()
