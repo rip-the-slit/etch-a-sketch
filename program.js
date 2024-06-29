bac8ba07c86819c7314d82ca413b30ac510423d2
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-06-29 12:53:46 -0430

    Arrange the grid

--- program.js
+++ program.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container")
 
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i < 256; i++) {
     const box = document.createElement("div")
     box.setAttribute("class", "box")
     container.appendChild(box)
