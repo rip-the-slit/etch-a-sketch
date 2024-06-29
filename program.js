546ac9f6429f82be49f771584475b4d9cb746d27
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-06-29 14:08:22 -0430

    Set up hover effect

--- program.js
+++ program.js
@@ -3,5 +3,9 @@ const container = document.querySelector(".container")
 for (let i = 0; i < 256; i++) {
     const box = document.createElement("div")
     box.setAttribute("class", "box")
+    box.addEventListener("mouseover", () => {
+        box.classList.add("painted")
+    })
     container.appendChild(box)
 }
+
